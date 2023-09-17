9250445c7bf7af0e3ad8fe3a6f2ce84b598c0821
Shirerpeton <hyloxalusfallax@gmail.com>
2023-09-17 19:53:59 +0400

    put str_buffer into its own struct with associated variables

--- tui.c
+++ tui.c
@@ -19,14 +19,21 @@ struct cell {
 
 typedef struct cell **buffer;
 
+struct str_buffer {
+    wchar_t *data;
+    size_t capacity;
+    size_t length;
+};
+
 void init_buffer(size_t rows, size_t cols, buffer *buf);
 void free_buffer(size_t rows, buffer *buf);
-void render_buffer(size_t rows, size_t cols, buffer *buf);
-void render_buffer_to_str(size_t rows, size_t cols, buffer *buf, size_t str_buf_size, wchar_t **str_buf);
+void init_str_buffer(size_t capacity, struct str_buffer *str_buffer);
+void free_str_buffer(struct str_buffer *str_buffer);
+void render_buffer_to_str(size_t rows, size_t cols, buffer *buf, struct str_buffer *str_buffer);
 
 const struct color DEFAULT_FG_COLOR = { .r = 255, .g = 255, .b = 255 };
 const struct color DEFAULT_BG_COLOR = { .r = 0, .g = 0, .b = 0 };
-const bool NO_DEFAULT_BG_COLOR = true; 
+const bool NO_DEFAULT_BG_COLOR = false; 
 const size_t ROWS = 24; 
 const size_t COLS = 80; 
 
@@ -35,16 +42,17 @@ int main() {
     fputws(L"\e[?1049h", stdout);
     fflush(stdout);
     size_t str_buf_size = ROWS * COLS * sizeof(wchar_t) * 20 + 1;
-    wchar_t *str_buf = malloc(str_buf_size); 
+    struct str_buffer str_buf;
+    init_str_buffer(str_buf_size, &str_buf);
     buffer buf;
     init_buffer(ROWS, COLS, &buf);
     buf[10][10].character = L'E';
-    render_buffer_to_str(ROWS, COLS, &buf, str_buf_size, &str_buf);
-    fputws(str_buf, stdout);
+    render_buffer_to_str(ROWS, COLS, &buf, &str_buf);
+    fputws(str_buf.data, stdout);
     fflush(stdout);
     getc(stdin);
     free_buffer(ROWS, &buf);
-    free(str_buf);
+    free_str_buffer(&str_buf);
     fputws(L"\e[?1049l", stdout);
 
     return 0;
@@ -69,72 +77,51 @@ void free_buffer(size_t rows, buffer *buf) {
     free(*buf);
 }
 
+void init_str_buffer(size_t capacity, struct str_buffer *str_buf) {
+    str_buf->capacity = capacity;
+    str_buf->length = 0;
+    str_buf->data = malloc(capacity);
+}
+
+void free_str_buffer(struct str_buffer *str_buf) {
+    free(str_buf->data);
+}
+
 bool eq_colors(struct color *first, struct color *second) {
     return first->r == second->r &&
         first->g == second->g &&
         first->b == second->b;
 }
 
-void render_buffer(size_t rows, size_t cols, buffer *buf) {
-    fputs("\e[0m", stdout);
-    fputs("\e[2J", stdout);
-    fputs("\e[H", stdout);
-    printf("\e[38;2;%d;%d;%dm", DEFAULT_FG_COLOR.r, DEFAULT_FG_COLOR.g, DEFAULT_FG_COLOR.b);
-    if(!NO_DEFAULT_BG_COLOR) {
-        printf("\e[48;2;%d;%d;%dm", DEFAULT_BG_COLOR.r, DEFAULT_BG_COLOR.g, DEFAULT_BG_COLOR.b);
-    } 
-    struct color prev_fg_color = DEFAULT_FG_COLOR;
-    struct color prev_bg_color = DEFAULT_BG_COLOR;
-    for(size_t i = 0; i < rows; i++) {
-        for(size_t j = 0; j < cols; j++) {
-            struct cell *cur_cell = &((*buf)[i][j]);
-            if(!eq_colors(&(cur_cell->fg_color), &prev_fg_color)) {
-                printf("\e[38;2;%d;%d;%dm", cur_cell->fg_color.r, cur_cell->fg_color.g, cur_cell->fg_color.b);
-            }
-            if(!eq_colors(&(cur_cell->bg_color), &prev_bg_color)) {
-                if(NO_DEFAULT_BG_COLOR && eq_colors(&(cur_cell->bg_color), (struct color *)&DEFAULT_BG_COLOR)) {
-                    fputs("\e[49m", stdout);
-                } else {
-                    printf("\e[48;2;%d;%d;%dm", cur_cell->bg_color.r, cur_cell->bg_color.g, cur_cell->bg_color.b);
-                }
-            }
-            fputc(cur_cell->character, stdout);
-        }
-        putc('\n', stdout);
-    }
+void append_to_str(const wchar_t *src, struct str_buffer *str_buf) {
+    wcscpy(str_buf->data + str_buf->length, src);
+    str_buf->length += wcslen(src);
 }
 
-void append_to_str(const wchar_t *src, wchar_t **dest, size_t *offset) {
-    wcscpy(*dest + *offset, src);
-    *offset += wcslen(src);
+void append_char_to_str(const wchar_t src, struct str_buffer *str_buf) {
+    str_buf->data[str_buf->length] = src;
+    str_buf->length++;
 }
 
-void append_char_to_str(const wchar_t src, wchar_t **dest, size_t *offset) {
-    *(*dest + *offset) = src;
-    (*offset)++;
-}
-
-void append_color_to_str(const wchar_t *format, wchar_t **dest, size_t dest_size, size_t *offset, struct color color) {
-    size_t written = swprintf(*dest + *offset,
-            dest_size - *offset,
+void append_color_to_str(const wchar_t *format, struct str_buffer *str_buf, struct color color) {
+    size_t written = swprintf(str_buf->data + str_buf->length,
+            str_buf->capacity - str_buf->length,
             format,
             color.r,
             color.g,
             color.b);
     if(written < 0) {
     } else {
-        *offset += written;
+        str_buf->length += written;
     }
 }
 
-void render_buffer_to_str(size_t rows, size_t cols, buffer *buf, size_t str_buf_size, wchar_t **str_buf) {
+void render_buffer_to_str(size_t rows, size_t cols, buffer *buf, struct str_buffer *str_buf) {
     size_t offset = 0;
-    append_to_str(L"\e[0m", str_buf, &offset);
-    append_to_str(L"\e[2J", str_buf, &offset);
-    append_to_str(L"\e[H", str_buf, &offset);
-    append_color_to_str(L"\e[38;2;%d;%d;%dm", str_buf, str_buf_size, &offset, DEFAULT_FG_COLOR);
+    append_to_str(L"\e[0m\e[2J\e[H", str_buf);
+    append_color_to_str(L"\e[38;2;%d;%d;%dm", str_buf, DEFAULT_FG_COLOR);
     if(!NO_DEFAULT_BG_COLOR) {
-        append_color_to_str(L"\e[48;2;%d;%d;%dm", str_buf, str_buf_size, &offset, DEFAULT_BG_COLOR);
+        append_color_to_str(L"\e[48;2;%d;%d;%dm", str_buf, DEFAULT_BG_COLOR);
     } 
     struct color prev_fg_color = DEFAULT_FG_COLOR;
     struct color prev_bg_color = DEFAULT_BG_COLOR;
@@ -142,17 +129,17 @@ void render_buffer_to_str(size_t rows, size_t cols, buffer *buf, size_t str_buf_
         for(size_t j = 0; j < cols; j++) {
             struct cell *cur_cell = &((*buf)[i][j]);
             if(!eq_colors(&(cur_cell->fg_color), &prev_fg_color)) {
-                append_color_to_str(L"\e[38;2;%d;%d;%dm", str_buf, str_buf_size, &offset, cur_cell->fg_color);
+                append_color_to_str(L"\e[38;2;%d;%d;%dm", str_buf, cur_cell->fg_color);
             }
             if(!eq_colors(&(cur_cell->bg_color), &prev_bg_color)) {
                 if(NO_DEFAULT_BG_COLOR && eq_colors(&(cur_cell->bg_color), (struct color *)&DEFAULT_BG_COLOR)) {
-                    append_to_str(L"\e[49m", str_buf, &offset);
+                    append_to_str(L"\e[49m", str_buf);
                 } else {
-                    append_color_to_str(L"\e[48;2;%d;%d;%dm", str_buf, str_buf_size, &offset, cur_cell->bg_color);
+                    append_color_to_str(L"\e[48;2;%d;%d;%dm", str_buf, cur_cell->bg_color);
                 }
             }
-            append_char_to_str(cur_cell->character, str_buf, &offset);
+            append_char_to_str(cur_cell->character, str_buf);
         }
-        append_char_to_str(L'\n', str_buf, &offset);
+        append_char_to_str(L'\n', str_buf);
     }
 }
