4d878c203583fc2247ff56445dfa73847f3a5d31
Shirerpeton <hyloxalusfallax@gmail.com>
2023-09-17 13:48:27 +0400

    add flag to prevent usage of bg color

--- tui.c
+++ tui.c
@@ -25,6 +25,7 @@ void render_buffer(size_t rows, size_t cols, buffer *buf);
 
 const struct color DEFAULT_FG_COLOR = { .r = 255, .g = 255, .b = 255 };
 const struct color DEFAULT_BG_COLOR = { .r = 0, .g = 0, .b = 0 };
+const bool NO_DEFAULT_BG_COLOR = true; 
 
 const size_t ROWS = 24; 
 const size_t COLS = 80; 
@@ -74,7 +75,9 @@ void render_buffer(size_t rows, size_t cols, buffer *buf) {
     fputs("\e[2J", stdout);
     fputs("\e[H", stdout);
     printf("\e[38;2;%d;%d;%dm", DEFAULT_FG_COLOR.r, DEFAULT_FG_COLOR.g, DEFAULT_FG_COLOR.b);
-    printf("\e[48;2;%d;%d;%dm", DEFAULT_BG_COLOR.r, DEFAULT_BG_COLOR.g, DEFAULT_BG_COLOR.b);
+    if(!NO_DEFAULT_BG_COLOR) {
+        printf("\e[48;2;%d;%d;%dm", DEFAULT_BG_COLOR.r, DEFAULT_BG_COLOR.g, DEFAULT_BG_COLOR.b);
+    }
     for(size_t i = 0; i < rows; i++) {
         for(size_t j = 0; j < cols; j++) {
             struct cell *cur_cell = &((*buf)[i][j]);
@@ -82,7 +85,11 @@ void render_buffer(size_t rows, size_t cols, buffer *buf) {
                 printf("\e[38;2;%d;%d;%dm", cur_cell->fg_color.r, cur_cell->fg_color.g, cur_cell->fg_color.b);
             }
             if(!eq_colors(&(cur_cell->bg_color), &prev_bg_color)) {
-                printf("\e[48;2;%d;%d;%dm", cur_cell->bg_color.r, cur_cell->bg_color.g, cur_cell->bg_color.b);
+                if(NO_DEFAULT_BG_COLOR && eq_colors(&(cur_cell->bg_color), (struct color *)&DEFAULT_BG_COLOR)) {
+                    fputs("\e[49m", stdout);
+                } else {
+                    printf("\e[48;2;%d;%d;%dm", cur_cell->bg_color.r, cur_cell->bg_color.g, cur_cell->bg_color.b);
+                }
             }
             fputc(cur_cell->character, stdout);
         }
